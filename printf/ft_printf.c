614e11098e37fe7c1893092cea3d556c06dd1771
Jolyne Mangeot <jolyne.mangeot@learner.42.tech>
2025-11-27 13:05:22 +0100

    refactor(ft_printf): change callprints second arg from char * to char
    change on itoa are unknown

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,57 +6,56 @@
 /*   By: jmangeot <jmangeot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/15 14:52:46 by jmangeot          #+#    #+#             */
-/*   Updated: 2025/11/18 23:32:41 by jmangeot         ###   ########.fr       */
+/*   Updated: 2025/11/27 13:03:24 by jmangeot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	is_flag(char c)
+static int is_flag(char c)
 {
-	char	*flags;
+	char *flags;
 
-	flags = "cspdiuxX%";
+	flags = "cspdiuaxX%";
 	while (*flags)
 		if (*flags++ == c)
 			return (1);
 	return (0);
 }
 
-static int	call_prints(const char *format, va_list args)
+static int call_prints(char flag, va_list args)
 {
-	char	flag;
-
-	flag = *format;
 	if (flag == 'c')
 		return (ft_put_char(
-				va_arg(args, int)));
+			va_arg(args, int)));
 	else if (flag == 's')
 		return (ft_put_str(
-				va_arg(args, char *)));
+			va_arg(args, char *)));
+	else if (flag == 'a')
+		return (ft_put_str("hihi 42"));
 	else if (flag == 'd' || flag == 'i')
 		return (ft_put_signed(
-				va_arg(args, signed int), 10, "0123456789"));
+			va_arg(args, signed int), 10, "0123456789"));
 	else if (flag == 'u')
 		return (ft_put_unsigned(
-				va_arg(args, unsigned int), 10, "0123456789"));
+			va_arg(args, unsigned int), 10, "0123456789"));
 	else if (flag == 'x')
 		return (ft_put_signed(
-				va_arg(args, unsigned int), 16, "0123456789abcdef"));
+			va_arg(args, unsigned int), 16, "0123456789abcdef"));
 	else if (flag == 'X')
 		return (ft_put_signed(
-				va_arg(args, unsigned int), 16, "0123456789ABCDEF"));
+			va_arg(args, unsigned int), 16, "0123456789ABCDEF"));
 	else if (flag == 'p')
 		return (ft_put_ptr(
-				va_arg(args, unsigned long int)));
+			va_arg(args, unsigned long int)));
 	return (ft_put_char('%'));
 }
 
-int	ft_printf(const char *format, ...)
+int ft_printf(const char *format, ...)
 {
-	va_list	args;
-	size_t	w_len;
-	int		print_size;
+	va_list args;
+	size_t w_len;
+	int print_size;
 
 	if (!format)
 		return (0);
@@ -70,10 +69,9 @@ int	ft_printf(const char *format, ...)
 			format++;
 			w_len++;
 		}
-		write(1, format - w_len, w_len);
-		print_size += w_len;
+		print_size += write(1, format - w_len, w_len);
 		if (*format++ == '%')
-			print_size += call_prints(format++, args);
+			print_size += call_prints(*format++, args);
 		else
 			format--;
 	}
