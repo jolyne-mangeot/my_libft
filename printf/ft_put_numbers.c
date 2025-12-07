f92a14947f8c0556630c8f479e8c3b0cde812f33
Jolyne Mangeot <jolyne.mangeot@learner.42.tech>
2025-12-07 19:00:04 +0100

    feat(l and ll): make long int and long long int printable as signed and unsigned, make put functions unusable outside of context and may need fixing

--- printf/ft_put_numbers.c
+++ printf/ft_put_numbers.c
@@ -6,60 +6,48 @@
 /*   By: jmangeot <jmangeot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/06 18:34:26 by jmangeot          #+#    #+#             */
-/*   Updated: 2025/12/06 18:38:53 by jmangeot         ###   ########.fr       */
+/*   Updated: 2025/12/07 18:47:17 by jmangeot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_put_lsigned(signed long int number,
-	signed long int base, char *digits)
+void	ft_put_lsigned(signed long int number, signed long int base,
+	char *digits, ssize_t *print_size)
 {
-int	print_size;
-
-print_size = 0;
-if (number > base - 1 || number < (base * -1) + 1)
-print_size += ft_put_lsigned(number / base, base, digits);
-else if (number < 0)
-print_size = ft_put_char('-');
-if (number < 0)
-number = (number % base) * -1;
-return (print_size + write(1, &digits[number % base], 1));
+	if (number > base - 1 || number < (base * -1) + 1)
+		ft_put_lsigned(number / base, base, digits, print_size);
+	else if (number < 0)
+		ft_put_char('-', print_size);
+	if (number < 0)
+		number = (number % base) * -1;
+	ft_put_char(digits[number % base], print_size);
 }
 
-int	ft_put_lunsigned(unsigned long int number,
-		unsigned long int base, char *digits)
+void	ft_put_lunsigned(unsigned long int number, unsigned long int base,
+	char *digits, ssize_t *print_size)
 {
-int	print_size;
-
-print_size = 0;
-if (number > base - 1)
-print_size += ft_put_lunsigned(number / base, base, digits);
-return (print_size + write(1, &digits[number % base], 1));
+	if (number > base - 1)
+		ft_put_lunsigned(number / base, base, digits, print_size);
+	ft_put_char(digits[number % base], print_size);
 }
 
-int	ft_put_llsigned(signed long long int number,
-	signed long long int base, char *digits)
+void	ft_put_llsigned(signed long long int number,
+	signed long long int base, char *digits, ssize_t *print_size)
 {
-int	print_size;
-
-print_size = 0;
-if (number > base - 1 || number < (base * -1) + 1)
-print_size += ft_put_llsigned(number / base, base, digits);
-else if (number < 0)
-print_size = ft_put_char('-');
-if (number < 0)
-number = (number % base) * -1;
-return (print_size + write(1, &digits[number % base], 1));
+	if (number > base - 1 || number < (base * -1) + 1)
+		ft_put_llsigned(number / base, base, digits, print_size);
+	else if (number < 0)
+		ft_put_char('-', print_size);
+	if (number < 0)
+		number = (number % base) * -1;
+	ft_put_char(digits[number % base], print_size);
 }
 
-int	ft_put_llunsigned(unsigned long long int number,
-		unsigned long long int base, char *digits)
+void	ft_put_llunsigned(unsigned long long int number,
+	unsigned long long int base, char *digits, ssize_t *print_size)
 {
-int	print_size;
-
-print_size = 0;
-if (number > base - 1)
-print_size += ft_put_llunsigned(number / base, base, digits);
-return (print_size + write(1, &digits[number % base], 1));
+	if (number > base - 1)
+		ft_put_llunsigned(number / base, base, digits, print_size);
+	ft_put_char(digits[number % base], print_size);
 }
