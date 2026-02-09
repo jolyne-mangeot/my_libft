99dcffff11c3811979442db4e68a3429709ccc41
Jolyne Mangeot <jolyne.mangeot@gmail.com>
2026-02-09 17:19:57 +0100

    fix[strcmp]: change condition operant from && to ||

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: jmangeot <jmangeot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/09 14:34:36 by jmangeot          #+#    #+#             */
-/*   Updated: 2026/02/09 14:34:38 by jmangeot         ###   ########.fr       */
+/*   Updated: 2026/02/09 17:16:08 by jmangeot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ difference of value of char1 - char2, otherwise 0.
 */
 int	ft_strcmp(const char *s1, const char *s2)
 {
-	while (*s1 && *s2)
+	while (*s1 || *s2)
 		if (*s1++ != *s2++)
 			return ((unsigned char)*(s1 - 1) - (unsigned char)*(s2 - 1));
 	return (0);
